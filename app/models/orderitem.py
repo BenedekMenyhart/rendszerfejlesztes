a293193146c5e9658872a3a083332eae16ff7f15
Szahara-gn <202978207+Szahara-gn@users.noreply.github.com>
2025-03-19 15:33:52 +0100

    orderitem, storeditem

---
+++ app/models/orderitem.py
@@ -0,0 +1,23 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.extensions import db
+from app.models.order import Order
+from app.models.storeditem import StoredItem
+from app.models.shipment import Shipment
+
+
+class OrderItem(db.Model):
+    __tablename__ = 'order_items'
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"))
+    order: Mapped["Order"] = relationship(back_populates="orders")
+
+    stored_item_id: Mapped[int] = mapped_column(ForeingKey("stored_items.id")
+    stored_item: Mapped["StoredItem"] = relationship(back_populates="orders")
+
+    quantity: Mapped[int] = mapped_column(nullable=False)
+
+    shipment_id: Mapped[int] = mapped_column(ForeignKey("shipments.id"))
+    shipment: Mapped["Shipment"] = relationship(back_populates="shipments") 
---
+++ app/models/storeditem.py
@@ -0,0 +1,9 @@
+class StoredItem(db.Model):
+    tablename = 'stored_items'
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+    name: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    quantity_available: Mapped[int] = mapped_column(nullable=False)
+    quantity_reserved: Mapped[int] = mapped_column(nullable=False)
+    quantity_requested : Mapped[int] = mapped_column(nullable=False)
